66b8acbba6451def9db93c2f933ac462fe018cf0
Madushidikrukshika <87822812+Madushidikrukshika@users.noreply.github.com>
2025-02-05 14:23:12 +0530

    travels updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 
 <footer class="w-full text-white ">
     <div class="w-full  mx-auto px-6 py-10 lora- bg-cover" style="background-image:url('assetes/images/bg9.png'); background-blend-mode: overlay; background-color: rgba(0, 0, 0, 0.5);">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-12 px-8">
+        <div class="w-full flex md:flex-row flex-col px-8 md:space-y-0 space-y-4">
             <!-- Left Section -->
-            <div class="w-full flex flex-col md:space-y-0 space-y-4">
+            <div class="w-full md:w-1/3 flex flex-col md:space-y-0 space-y-4">
                 <h2 class="text-3xl font-cursive mb-4">Alluring Tours</h2>
                 <p class="text-gray-300 text-justify">
                 Alluring Tours is your trusted travel partner in Sri Lanka, offering unforgettable experiences across breathtaking landscapes, cultural wonders, and pristine beaches. Let us craft your perfect journey with tailor-made tour packages and expert local guides. Discover Sri Lanka like never before!
@@ -26,7 +26,8 @@
             </div>
 </div>
             <!-- Center Section -->
-            <div class="flex flex-col md:items-start items-center">
+            <div class="w-full md:w-1/3 flex flex-col md:items-center">
+                <div class="flex flex-col items-start">
                 <h3 class="text-lg font-semibold ">About</h3>
                 <div class="flex flex-col mt-4 space-y-2 ">
                     <a href="./index.php#packages" class="text-gray-300 hover:text-[#B9C37B]">Our Packages</a>
@@ -34,10 +35,11 @@
                     <a href="./index.php#experiences" class="text-gray-300 hover:text-[#B9C37B]">Key Experiences</a>
                 </div>
             </div>
+            </div>
             
 
             <!-- Right Section -->
-            <div class="w-full flex flex-col md:items-start items-center">
+            <div class="w-full md:w-1/3 flex flex-col items-start">
                 <h3 class="text-lg font-semibold">Contact Us</h3>
                 <p class="text-gray-300 mt-2">170/6,Kondagammulla,Demanhandiya</p>
                 <p class="text-gray-300 mt-2">+94 776509918 / +94 773212076</p>
