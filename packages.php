aa3a8aa016f72b4a7eca613f9d6d8ec7af3ccb01
Madushidikrukshika <87822812+Madushidikrukshika@users.noreply.github.com>
2025-02-06 16:32:36 +0530

    updated
    completed

---
+++ packages.php
@@ -0,0 +1,161 @@
+<?php include("./includes/header.php"); ?>
+
+<div id="packages" class="text-center bg-[#1E1E1E] py-12 px-6 md:px-12 lg:px-16">
+  <!-- Section Title -->
+  <h2 class="text-3xl md:text-5xl font-bold mb-4 text-[#B9C37B] lora-">OUR PACKAGES</h2>
+  <p class="text-lg md:text-xl lora- italic font-bold text-[#B9C37B]">Choose the perfect package for your journey.</p>
+
+  <!-- Packages Grid -->
+  <div class="w-full flex flex-col bg-[#1E1E1E] space-y-4 p-8 md:p-8 lora-">
+    
+    <!-- Card 1 -->
+    <div class="w-full flex md:flex-row flex-col border-2 border-[#B9C37B] p-4 md:p-6 hover:shadow-lg hover:scale-105 transition transform rounded-xl md:h-64">
+      <div class="w-full md:w-1/4">
+      <div class="w-full h-full flex flex-col items-center justify-center">
+        <h3 class="text-6xl md:text-9xl font-extrabold text-[#B9C37B]">01</h3>
+        <p class="text-lg md:text-xl text-[#B9C37B]">Travel Only</p>
+
+      </div>
+      </div>
+      
+      <div class="w-full flex flex-col justify-center items-center text-white md:mt-0 mt-4">
+        <div class="w-full flex italic md:text-xl">
+        <p>Type of Vehicle - </p>
+        <p>Hybrid Van/Car</p>
+        </div>
+        <div class="w-full flex flex-col justify-start items-start italic space-y-4 pt-8">
+          <div class="w-full flex justify-start items-center space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All transfer and local sightseeing from arrival day till   drop to Airport</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All local sightseeing in the same vehicle, every day, after breakfast till sunset</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>Driver’s Bata, road tax and parking charges.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+    <!-- Card 2 -->
+    <div class="w-full flex md:flex-row flex-col border-2 border-[#B9C37B] p-4 md:p-6 hover:shadow-lg hover:scale-105 transition transform rounded-xl md:h-64">
+      <div class="w-full md:w-1/4">
+      <div class="w-full h-full flex flex-col items-center justify-center">
+      <h3 class="text-6xl md:text-9xl font-extrabold text-[#B9C37B]">02</h3>
+      <div class="w-full flex flex-col text-lg md:text-xl text-[#B9C37B]">
+            <p>Travel</p>
+            <p>+</p>
+            <p>Accommodation</p>
+          </div>
+      </div>
+      </div>
+      
+      <div class="w-full flex flex-col justify-center items-center text-white">
+        <div class="w-full flex italic md:text-xl md:mt-0 mt-4">
+        <p>Type of Vehicle - </p>
+        <p>Hybrid Car (15 Days for 02 Adults)</p>
+        </div>
+        <div class="w-full flex justify-start items-center italic pt-4">
+        <p>Accommodations in  3 star hotels / 4 star hotels with swimming pools with breakfast.</p>
+
+        </div>
+        <div class="w-full flex flex-col justify-start items-start italic space-y-4 pt-8">
+          <div class="w-full flex justify-start items-center space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All transfer and local sightseeing from arrival day till   drop to Airport</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All local sightseeing in the same vehicle, every day, after breakfast till sunset</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>Driver’s Bata, road tax and parking charges.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <!-- Card 3 -->
+    
+    <div class="w-full flex md:flex-row flex-col border-2 border-[#B9C37B] p-4 md:p-6 hover:shadow-lg hover:scale-105 transition transform rounded-xl md:h-64">
+      <div class="w-full md:w-1/4">
+      <div class="w-full h-full flex flex-col items-center justify-center">
+      <h3 class="text-6xl md:text-9xl font-extrabold text-[#B9C37B]">03</h3>
+      <div class="w-full flex flex-col text-lg md:text-xl text-[#B9C37B]">
+            <p>Travel</p>
+            <p>+</p>
+            <p>Activities</p>
+          </div>
+      </div>
+      </div>
+      
+      <div class="w-full flex flex-col justify-center items-center text-white">
+        <div class="w-full flex italic md:text-xl md:mt-0 mt-4">
+        <p>Type of Vehicle - </p>
+        <p>Hybrid Van/Car</p>
+        </div>
+        <div class="w-full flex flex-col justify-start items-start italic space-y-4 pt-8">
+          <div class="w-full flex justify-start items-center space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All transfer and local sightseeing from arrival day till   drop to Airport</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All local sightseeing in the same vehicle, every day, after breakfast till sunset</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>Driver’s Bata, road tax and parking charges.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <!-- Card 4 -->
+    <div class="w-full flex md:flex-row flex-col border-2 border-[#B9C37B] p-4 md:p-6 hover:shadow-lg hover:scale-105 transition transform rounded-xl md:h-auto">
+      <div class="w-full md:w-1/4">
+      <div class="w-full h-full flex flex-col items-center justify-center">
+      <h3 class="text-6xl md:text-9xl font-extrabold text-[#B9C37B]">04</h3>
+      <div class="w-full flex flex-col text-lg md:text-xl text-[#B9C37B]">
+            <p>Travel</p>
+            <p>+</p>
+            <p>Accommodation</p>
+            <p>+</p>
+            <p>Activities</p>
+          </div>
+      </div>
+      </div>
+      
+      <div class="w-full flex flex-col justify-center items-center text-white">
+        <div class="w-full flex italic md:text-xl md:mt-0 mt-4">
+        <p>Type of Vehicle - </p>
+        <p>Hybrid Van/Car</p>
+        </div>
+        <div class="w-full flex justify-start items-center italic pt-4">
+        <p>Accommodations in  3 star hotels / 4 star hotels with swimming pools with breakfast.</p>
+
+        </div>
+        <div class="w-full flex flex-col justify-start items-start italic space-y-4 pt-8">
+          <div class="w-full flex justify-start items-center space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All transfer and local sightseeing from arrival day till   drop to Airport</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>All local sightseeing in the same vehicle, every day, after breakfast till sunset</p>
+          </div>
+          <div class="w-full flex space-x-4">
+          <p class="text-sm"><i class="ri-circle-fill"></i></p>
+          <p>Driver’s Bata, road tax and parking charges.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
